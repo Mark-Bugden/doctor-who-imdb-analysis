9109a565bb20cae3e1be668e88ff02490ab1881a
Mark Bugden <mathphys@mark-bugden.com>
2025-04-10 11:01:35 +0200

    Updated for Gatwa Season 1

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 def prepare_contributor_data(df, column, min_episodes=3):
     """
     Explodes the given column (e.g. 'writers' or 'directors') in the DataFrame,
@@ -9,24 +7,27 @@ def prepare_contributor_data(df, column, min_episodes=3):
     Returns a DataFrame with: rating, doctor, contributor, role, weight
     """
     # Explode the column
-    exploded = df[['rating', 'doctor', column]].copy()
-    exploded[column] = exploded[column].str.split(',').apply(lambda x: [i.strip() for i in x])
-    exploded = exploded.explode(column).rename(columns={column: 'contributor'})
-    
+    exploded = df[["rating", "doctor", column]].copy()
+    exploded[column] = (
+        exploded[column].str.split(",").apply(lambda x: [i.strip() for i in x])
+    )
+    exploded = exploded.explode(column).rename(columns={column: "contributor"})
+
     # Count contributions
-    contributor_counts = exploded['contributor'].value_counts()
+    contributor_counts = exploded["contributor"].value_counts()
     valid_contributors = contributor_counts[contributor_counts >= min_episodes].index
 
     # Filter contributors
-    exploded = exploded[exploded['contributor'].isin(valid_contributors)].copy()
+    exploded = exploded[exploded["contributor"].isin(valid_contributors)].copy()
 
     # Compute weights per episode
-    contrib_per_episode = exploded.groupby(exploded.index).contributor.transform('count')
-    exploded['weight'] = 1 / contrib_per_episode
-    exploded['role'] = column[:-1]  # 'writers' -> 'writer', 'directors' -> 'director'
+    contrib_per_episode = exploded.groupby(exploded.index).contributor.transform(
+        "count"
+    )
+    exploded["weight"] = 1 / contrib_per_episode
+    exploded["role"] = column[:-1]  # 'writers' -> 'writer', 'directors' -> 'director'
 
-    return exploded[['rating', 'doctor', 'contributor', 'role', 'weight']]
-
+    return exploded[["rating", "doctor", "contributor", "role", "weight"]]
 
 
 classic_season_to_doctor_mapping = {
@@ -76,18 +77,28 @@ modern_season_to_doctor_mapping = {
     15: "Fifteenth Doctor",
 }
 
+gatwa_season_to_doctor_mapping = {
+    1: "Fifteenth Doctor",
+    2: "Fifteenth Doctor",
+}
+
+gatwa_season_to_modern_season_mapping = {
+    1: 14,
+    2: 15,
+}
+
 doctor_to_actor_mapping = {
     "First Doctor": "William Hartnell",
-    "Second Doctor": "Patrick Troughton", 
+    "Second Doctor": "Patrick Troughton",
     "Third Doctor": "Jon Pertwee",
     "Fourth Doctor": "Tom Baker",
     "Fifth Doctor": "Peter Davison",
     "Sixth Doctor": "Colin Baker",
     "Seventh Doctor": "Sylvester McCoy",
     "Ninth Doctor": "Christopher Eccleston",
-    "Tenth Doctor": "David Tennant", 
+    "Tenth Doctor": "David Tennant",
     "Eleventh Doctor": "Matt Smith",
     "Twelfth Doctor": "Peter Capaldi",
     "Thirteenth Doctor": "Jodie Whittaker",
-    "Fifteenth Doctor": "Ncuti Gatwa",   
+    "Fifteenth Doctor": "Ncuti Gatwa",
 }
